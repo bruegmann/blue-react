9d25538b67bb413ec3a1732d4959def7ef08437c
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-07-02 13:07:38 +0200

    Built docs

---
+++ docs/precache-manifest.5deb75ddfaa499c286b740028fe0897a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a911d8e0a636ae563fd13f0c26011f5",
+    "url": "/blue-react/index.html"
+  },
+  {
+    "revision": "5b07712510a78b9281be",
+    "url": "/blue-react/static/css/main.3367e856.chunk.css"
+  },
+  {
+    "revision": "528e7dc156d386316eca",
+    "url": "/blue-react/static/js/2.a6dfe78f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blue-react/static/js/2.a6dfe78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b07712510a78b9281be",
+    "url": "/blue-react/static/js/main.061b00e8.chunk.js"
+  },
+  {
+    "revision": "80dce4c8a24ac68266a9",
+    "url": "/blue-react/static/js/runtime-main.7d9a63dc.js"
+  },
+  {
+    "revision": "78dd83b0f0affdacf5172008538b4d31",
+    "url": "/blue-react/static/media/blue.78dd83b0.svg"
+  }
+]);
