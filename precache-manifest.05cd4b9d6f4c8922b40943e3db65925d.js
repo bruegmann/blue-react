66cb3134cc48ae094ba802dba1b4ee7088803af6
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-12-11 08:08:53 +0100

    Updates

--- precache-manifest.05cd4b9d6f4c8922b40943e3db65925d.js
+++ precache-manifest.05cd4b9d6f4c8922b40943e3db65925d.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "27dc4c4b3d9d4e53d830c696c725e199",
+    "revision": "a9dd3c484d5070dc43e2ac0085f22573",
     "url": "/blue-react/index.html"
   },
   {
-    "revision": "48a982a14381e860b99a",
+    "revision": "c22c04cf924b6e7ffe47",
     "url": "/blue-react/static/css/2.a55bcd37.chunk.css"
   },
   {
-    "revision": "6cb8036a724c6fd509b8",
+    "revision": "5c6e670d47f88a6266d9",
     "url": "/blue-react/static/css/main.8378afa8.chunk.css"
   },
   {
-    "revision": "48a982a14381e860b99a",
-    "url": "/blue-react/static/js/2.6717f820.chunk.js"
+    "revision": "c22c04cf924b6e7ffe47",
+    "url": "/blue-react/static/js/2.3ba41dbe.chunk.js"
   },
   {
     "revision": "d1fca7bf05980f44871f5039d6a4a1d9",
-    "url": "/blue-react/static/js/2.6717f820.chunk.js.LICENSE.txt"
+    "url": "/blue-react/static/js/2.3ba41dbe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6cb8036a724c6fd509b8",
-    "url": "/blue-react/static/js/main.32787eec.chunk.js"
+    "revision": "5c6e670d47f88a6266d9",
+    "url": "/blue-react/static/js/main.a6d8427a.chunk.js"
   },
   {
     "revision": "80dce4c8a24ac68266a9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blue-react/precache-manifest.a733d61fafb91d54f265bc51d4b90914.js"
+  "/blue-react/precache-manifest.05cd4b9d6f4c8922b40943e3db65925d.js"
 );
 
 self.addEventListener('message', (event) => {
