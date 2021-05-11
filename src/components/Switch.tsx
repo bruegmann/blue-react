6085488930181a2e954e530282bdba41e9e96db9
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2021-05-11 14:55:37 +0200

    Removed semicolons

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,21 +1,21 @@
 import React, { createElement } from "react"
 
 export interface SwitchProps {
-    className?: string;
-    checked?: boolean;
-    onChange?: (event: React.ChangeEvent) => void;
+    className?: string
+    checked?: boolean
+    onChange?: (event: React.ChangeEvent) => void
 
     /**
      * Sets label inside of the switch. If you set this, you should also set the className "lg" to make the switch larger.
      */
-    sliderLabel?: string;
+    sliderLabel?: string
 
     /**
      * You can change the type of the wrapper element. If you do, the `onChange` event might not be triggered.
      */
-    elementType?: string;
+    elementType?: string
 
-    disabled?: boolean;
+    disabled?: boolean
 }
 
 /**
