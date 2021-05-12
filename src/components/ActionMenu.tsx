2d77e71d0422c5cf6c30c410fa32f2f1aea8b9dd
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2021-05-12 11:41:07 +0200

    #64 Moved code from HeaderActions to ActionMenu

--- src/components/ActionMenu.tsx
+++ src/components/ActionMenu.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import MenuItem from "./MenuItem"
+import { breakOption } from "./shared"
 import Utilities from "./Utilities"
 
 export interface ActionMenuProps {
@@ -14,13 +15,19 @@ export interface ActionMenuProps {
      * Icon component for the toggle icon.
      */
     toggleIcon?: any
+
+    className?: string
+    break?: breakOption
 }
 
 /**
  * <span class="badge badge-info">Info</span> You can also use <strong>Actions</strong> instead.<br>
  * The Action Menu on the top right of a page. You can place Actions here which are in context of the current page.
  */
-export default function ActionMenu({ hideToggleAction, children, toggleIcon = "bi-iconmonstr-menu-7" }: ActionMenuProps) {
+export default function ActionMenu(props: ActionMenuProps) {
+    const { hideToggleAction, children, className, toggleIcon = "bi-iconmonstr-menu-7" } = props
+    const _break = props.break || "lg"
+
     const [actionsToggledIn, setActionsToggledIn] = useState<boolean>(false)
 
     const toggleActions = () => {
@@ -50,20 +57,23 @@ export default function ActionMenu({ hideToggleAction, children, toggleIcon = "b
     }, [])
 
     return (
-        <ul className="blue-app-actions-menu nav navbar-nav navbar-right fluent-btn">
-            <div className="fluent-btn-ball" />
+        <div className={`blue-app-actions navbar ${className} navbar-expand-${_break} ${_break}`}>
+            <div className="ui-header-wrapper" onClick={Utilities.scrollToTop} />
+            <ul className="blue-app-actions-menu nav navbar-nav navbar-right fluent-btn">
+                <div className="fluent-btn-ball" />
 
-            {!actionsToggledIn && !hideToggleAction ?
-                <MenuItem
-                    className="blue-app-actions-menu-toggle"
-                    onClick={() => toggleActions()}
-                    icon={toggleIcon}
-                    aria-label="Toggle menu"
-                />
-                : ""
-            }
+                {!actionsToggledIn && !hideToggleAction ?
+                    <MenuItem
+                        className="blue-app-actions-menu-toggle"
+                        onClick={() => toggleActions()}
+                        icon={toggleIcon}
+                        aria-label="Toggle menu"
+                    />
+                    : ""
+                }
 
-            {children}
-        </ul>
+                {children}
+            </ul>
+        </div>
     )
 }
