9f6418a99fcd878a1ec0dea4cba551596a7c51fc
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2021-07-14 12:29:25 +0200

    New prop resetIcon for Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -22,6 +22,11 @@ export interface SearchProps {
     reset?: boolean
 
     /**
+     * Define custom icon for the reset button.
+     */
+    resetIcon?: any
+
+    /**
      * Is component inside of the sidebar?
      */
     sidebar?: boolean
@@ -33,7 +38,7 @@ export interface SearchProps {
  * A search bar that can be placed to the sidebar or on a page.
  */
 export default function Search(props: SearchProps) {
-    const { autoFocus, body, className, icon, onChange, onSubmit, placeholder, reset, sidebar } = props
+    const { autoFocus, body, className, icon, onChange, onSubmit, placeholder, reset, resetIcon, sidebar } = props
     const SearchControlId = "blue-app-search-control-" + Utilities.guid()
 
     const [value, setValue] = useState<string>(props.value || "")
@@ -106,7 +111,16 @@ export default function Search(props: SearchProps) {
                                     } as any)
                             }}
                         >
-                            <span className="bi-navigate_cross" />
+                            {resetIcon || <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="1em"
+                                height="1em"
+                                fill="currentColor"
+                                className="bi bi-x mt-n1"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M4.646 4.646a.5.5 0 01.708 0L8 7.293l2.646-2.647a.5.5 0 01.708.708L8.707 8l2.647 2.646a.5.5 0 01-.708.708L8 8.707l-2.646 2.647a.5.5 0 01-.708-.708L7.293 8 4.646 5.354a.5.5 0 010-.708z"></path>
+                            </svg>}
                         </button>
                     </div>
                 }
