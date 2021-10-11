03e3388cf41345ca9d33b03166b03b36516eac35
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2021-10-11 13:47:58 +0200

    Optimized DocumentView

--- src/docs/examples/DocumentView.js
+++ src/docs/examples/DocumentView.js
@@ -6,29 +6,8 @@ class DocumentViewExample extends React.Component {
         return (
             <div>
                 <DocumentView
-                    content={`<!DOCTYPE html>
-                    <html lang="en">
-                    
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>Demo Document</title>
-                    
-                        <style type="text/css">
-                            body {
-                                background-color: #f2f2f2;
-                                color: #222;
-                                font-family: Arial, Helvetica, sans-serif;
-                            }
-                        </style>
-                    </head>
-                    
-                    <body>
-                        <h1>Hey, this is HTML content</h1>
-                    </body>
-                    
-                    </html>`}
+                    src="/blue-react/v7/example-document.pdf"
+                    mimeType="application/pdf"
                 />
             </div>
         )
