6df6ee6027b45d9cecc48e8a178eed0cc82516ae
lgk-bsw <lgk@patorg.de>
2022-07-22 11:45:05 +0200

    #108 Build

--- dist/components/ActionMenu.js
+++ dist/components/ActionMenu.js
@@ -39,7 +39,16 @@ function ActionMenu(props) {
       children = props.children,
       className = props.className,
       _props$toggleIcon = props.toggleIcon,
-      toggleIcon = _props$toggleIcon === void 0 ? "bi-iconmonstr-menu-7" : _props$toggleIcon;
+      toggleIcon = _props$toggleIcon === void 0 ? /*#__PURE__*/_react.default.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "16",
+    height: "16",
+    fill: "currentColor",
+    className: "bi bi-three-dots-vertical",
+    viewBox: "0 0 16 16"
+  }, /*#__PURE__*/_react.default.createElement("path", {
+    d: "M9.5 13a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm0-5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm0-5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+  })) : _props$toggleIcon;
 
   var _break = props.break || "lg";
 
