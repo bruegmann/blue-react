dca42e3b5816f28c75ea0ef034ae8ca39dadaae7
[nku-bsw] <[nku@patorg.de]>
2022-11-10 09:51:32 +0100

    builded new docs & types

--- dist/types/components/ModalProvider.d.ts
+++ dist/types/components/ModalProvider.d.ts
@@ -4,8 +4,8 @@ export interface ModalProviderProps {
 }
 declare const ModalProvider: ({ children, ...rest }: ModalProviderProps) => JSX.Element;
 declare const useModal: () => {
-    ask: (text: string, title?: string) => Promise<string | boolean>;
-    tell: (text: string, title?: string) => Promise<boolean>;
-    verify: (text: string, title?: string) => Promise<boolean>;
+    ask: (text: string, title?: string, icon?: HTMLElement | any) => Promise<string | boolean>;
+    tell: (text: string, title?: string, icon?: HTMLElement | any) => Promise<boolean>;
+    verify: (text: string, title?: string, icon?: HTMLElement | any) => Promise<boolean>;
 };
 export { ModalProvider, useModal };
