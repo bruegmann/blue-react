21b594c26d2008a6235b0a95c0929bd02cfd382a
lgk-bsw <lgk@patorg.de>
2025-08-08 11:24:24 +0200

    chore: update dependencies in package.json and package-lock.json; improve accessibility in Layout component; refactor StackblitzEmbed component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -31,6 +31,7 @@ export default function Layout({
                 className="blue-layout-toggle blue-layout-toggle-expand"
                 htmlFor="layout-expand"
                 title={getPhrase("Toggle menu")}
+                aria-label={getPhrase("Toggle menu")}
                 role="button"
             >
                 <span className="btn blue-menu-item w-100 h-100 d-flex align-items-center justify-content-center p-0">
@@ -38,16 +39,12 @@ export default function Layout({
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 16 16"
                         aria-hidden="true"
-                        tabIndex={-1}
                         width="1em"
                         height="1em"
                         fill="currentColor"
                         className="bi bi-layout-sidebar"
                     >
-                        <path
-                            tabIndex={-1}
-                            d="M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5-1v12h9a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM4 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h2z"
-                        ></path>
+                        <path d="M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5-1v12h9a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM4 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h2z"></path>
                     </svg>
                 </span>
             </label>
@@ -61,6 +58,7 @@ export default function Layout({
                 className="blue-layout-toggle blue-layout-toggle-drawer"
                 htmlFor="layout-drawer"
                 title={getPhrase("Toggle menu")}
+                aria-label={getPhrase("Toggle menu")}
                 role="button"
             >
                 <span className="btn blue-menu-item w-100 h-100 d-flex align-items-center justify-content-center p-0">
@@ -68,20 +66,13 @@ export default function Layout({
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 16 16"
                         aria-hidden="true"
-                        tabIndex={-1}
                         width="1em"
                         height="1em"
                         fill="currentColor"
                         className="bi bi-layout-sidebar-inset"
                     >
-                        <path
-                            tabIndex={-1}
-                            d="M14 2a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1zM2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2z"
-                        ></path>
-                        <path
-                            tabIndex={-1}
-                            d="M3 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1z"
-                        ></path>
+                        <path d="M14 2a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1zM2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2z"></path>
+                        <path d="M3 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1z"></path>
                     </svg>
                 </span>
             </label>
--- src/docs/components/StackblitzEmbed.tsx
+++ src/docs/components/StackblitzEmbed.tsx
@@ -1,57 +1,37 @@
-import { useState } from "react"
-import { PencilSquare, PlayFill } from "react-bootstrap-icons"
+import { PencilSquare } from "react-bootstrap-icons"
 import image from "../images/stackblitz.png"
 
 export default function StackblitzEmbed() {
-    const [loadIframe, setLoadIframe] = useState<boolean>(false)
-
     return (
-        <>
-            {loadIframe ? (
-                <div className="ratio ratio-16x9 bg-light rounded">
-                    <iframe
-                        title="Stackblitz"
-                        className="rounded"
-                        src="https://stackblitz.com/edit/blue-react-app-stackblitz-qavjnx?embed=1&file=index.tsx&hideExplorer=1"
-                    />
-                </div>
-            ) : (
-                <div
-                    className="bg-primary neu-shadow-4 text-white rounded p-4 d-flex justify-content-between"
-                    style={{ backgroundImage: `url(${image})` }}
-                >
-                    <div>
-                        <h5>Do you want to load external content from Stackblitz?</h5>
-                        <p>It will contain a live editor, which lets you edit a demo app with Blue React.</p>
+        <div
+            className="bg-primary neu-shadow-4 text-white rounded p-4 d-flex justify-content-between"
+            style={{ backgroundImage: `url(${image})` }}
+        >
+            <div>
+                <h2 className="h5">
+                    Try Blue React right in your browser with Stackblitz
+                </h2>
 
-                        <button
-                            className="btn btn-light d-inline-flex align-items-center"
-                            onClick={() => setLoadIframe(true)}
-                        >
-                            <PlayFill className="me-1" /> Load content
-                        </button>
-                        <a
-                            className="btn btn-link text-white"
-                            href="https://stackblitz.com/edit/blue-react-app-stackblitz-qavjnx?"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Open content in new tab
-                        </a>
-                        <a
-                            className="btn btn-link text-white"
-                            href="https://stackblitz.com/privacy-policy"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Privacy Policy by Stackblitz
-                        </a>
-                    </div>
-                    <h1 className="display-1">
-                        <PencilSquare />
-                    </h1>
-                </div>
-            )}
-        </>
+                <a
+                    className="btn btn-light"
+                    href="https://stackblitz.com/~/github.com/bruegmann/vite-template-blue-react"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Open content in new tab
+                </a>
+                <a
+                    className="btn btn-link text-white"
+                    href="https://stackblitz.com/privacy-policy"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Privacy Policy by Stackblitz
+                </a>
+            </div>
+            <h1 className="display-3">
+                <PencilSquare />
+            </h1>
+        </div>
     )
 }
