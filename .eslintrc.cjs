f3bb62fa6432b7abbdfeba6a80acc451f57b5ffc
lgk-bsw <lgk@patorg.de>
2025-11-14 09:02:31 +0100

    Update Blue-Web & Install React Compiler

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended"
+        "plugin:react-hooks/recommended-latest"
     ],
     ignorePatterns: ["dist", ".eslintrc.cjs"],
     parser: "@typescript-eslint/parser",
