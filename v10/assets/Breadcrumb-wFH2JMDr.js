0119a88a378fdfc65a3d340ce886fbb3841b8f51
lgk-bsw <lgk@patorg.de>
2025-11-25 12:15:01 +0100

    Updates

--- v10/assets/Breadcrumb-wFH2JMDr.js
+++
@@ -1,1 +0,0 @@
-import{j as e,H as t,L as r}from"./index-Cv_8C3gj.js";function n(){return e.jsx(t,{breadcrumb:[e.jsx(r,{to:"/",children:"Home page"}),e.jsx(r,{to:"/component/HeaderTitle",children:"This component"}),"Current page"]})}export{n as default};
